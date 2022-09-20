301d0b7e0551ed7f1e0c4680bffb82bb423b8b2f
Unpei2 <r.ho5@share.epsb.ca>
2022-09-20 14:49:15 -0600

    i hate this game

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let tasksEl = document.getElementById('tasks');
 
+
+
+// global variables
+let tasks = [];
+displayAll();
+
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
 
@@ -25,7 +31,11 @@ function goBtnHandler() {
 
 // MENU FUNCTIONS
 function addTask() {
-  console.log('Add Task');
+  let description = prompt("Enter task description:")
+  tasks.push(newtask(description));
+  savetasks();
+  displayAll();
+
 }
 
 function toggleTask() {
@@ -38,4 +48,40 @@ function removeTask() {
 
 function clearAll() {
   console.log('Clear All');
+
+}
+
+
+// Helper Functions
+function newtask(taskDescription){
+  return {
+    description: taskDescription,
+    completed: '',
+  };
+}
+
+// Display all tasks in global tasks array
+function displayAll(){
+  let outputStr = '';
+  for (let i = 0;i < tasks.length; i++){
+    outputStr += getTasksHTMLStr(tasks[i], i)
+  }
+  tasksEl.innerHTML = outputStr;
+}
+
+function getTasksHTMLStr(task, i){
+  return `
+    <div>
+      ${i}: ${task.description}
+    </div>
+  `;
+}
+// save global tasks to local storage
+function savetasks(){
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+} 
+
+function loadTasks(){
+  let tasksStr = localStorage.getItem("tasks");
+  return JSON.parse(tasksStr)
 }
